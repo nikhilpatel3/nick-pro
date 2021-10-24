2a87f77553b1ffd21b45d904ecd0b59fd25394df
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-10-24 16:57:38 +0500

    Update old.py

--- abm-pro/old/old.py
+++ abm-pro/old/old.py
@@ -5,7 +5,7 @@ import os
 import platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from abm import abm_mix
+    from abm64 import abm_mix
     abm_mix()
 elif bit == '32bit':
     from abm32 import abm_mix

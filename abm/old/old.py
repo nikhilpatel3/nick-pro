1920780093b869123a1a017a2957d2bb7005602a
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-10-28 20:48:34 +0500

    Create old.py

---
+++ abm/old/old.py
@@ -0,0 +1,15 @@
+import os
+import re
+import time
+
+os.system("clear")
+print('')
+print('')
+print('')
+print('')
+print("This old script has been deleted").center(50)
+print("You can not use this old method").center(50)
+time.sleep(1)
+print('')
+raw_input("\tPress enter to back")
+os.system("cd - && python2 abm.py")

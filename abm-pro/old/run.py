000b222bdb794f2c8931f39082b50a9e1dff32d8
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-10-29 18:38:28 +0500

    Create run.py

---
+++ abm-pro/old/run.py
@@ -0,0 +1,14 @@
+import os
+import re
+
+os.system("clear")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("This old commnad has been deleted").center(50)
+print("You can not use this tool").center(50)
+print("")
+raw_input("\tpress enter to back")
+os.system("cd - && python2 abm-pro.py")
